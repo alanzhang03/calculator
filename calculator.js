fa4c650d3e637e594e38baaa2bc130e64c2a2958
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-22 15:18:17 -0400

    operations

--- calculator.js
+++ calculator.js
@@ -1,5 +1,24 @@
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+function mutiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1,num2){
+    return num1/num2;
+}
 
 
 const calcContainer = document.getElementById("calculator-container");
 
 
+console.log(add(1,2));
+console.log(mutiply(6,2));
+console.log(subtract(6,2));
+console.log(divide(3,0));
