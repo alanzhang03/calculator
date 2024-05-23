0537bf3569244c1aa39cbdc9a280356f22ac136f
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-23 12:16:20 -0400

    display

--- calculator.js
+++ calculator.js
@@ -14,3 +14,11 @@ function divide(num1,num2){
     return num1/num2;
 }
 
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let resultDisplayed = false;
+
+
+const numbers = document.querySelector('.numbers')
+numbers.addEventListener('onclick', )
