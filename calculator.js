d479b6c5c9092c8b84975f132d957ac18133573e
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-23 16:37:51 -0400

    functions

--- calculator.js
+++ calculator.js
@@ -1,24 +1,41 @@
-function add(num1,num2){
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1,num2){
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function mutiply(num1,num2){
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1,num2){
-    return num1/num2;
+function divide(num1, num2) {
+    return num1 / num2;
 }
 
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let resultDisplayed = false;
+const display = document.getElementById('calculatorDisplay');
 
+function appendToDisplay(input) {
+    display.value += input;
+}
 
-const numbers = document.querySelector('.numbers')
-numbers.addEventListener('onclick', )
+function clearDisplay() {
+    display.value = '';
+}
+
+function calcPercentage(){
+    display.value = display.value/100;
+}
+
+function changeSign(){
+    display.value = display.value * -1;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
