e22a0aa6a0201f3b5d07774e55be18a2281cc855
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-23 11:49:58 -0400

    buttons

--- calculator.js
+++ calculator.js
@@ -14,21 +14,3 @@ function divide(num1,num2){
     return num1/num2;
 }
 
-const functions = ['+', '-', '*', '/']
-const nums = ['1', '2', '3', '4','5','6','7','8','9','0',]
-
-const functionButtonsContainer = document.querySelector("function-buttons-container")
-const numButtonsContainer = document.querySelector("num-buttons-container")
-
-functionButtonsContainer.forEach(symbol, ()=>{
-    const button = document.createElement('button')
-    button.textContent = symbol
-    functionButtonsContainer.appendChild(button);
-})
-
-numButtonsContainer.forEach(number, ()=>{
-    const numButton = document.createElement('button')
-    numButton.textContent = number
-    numButtonsContainer.appendChild(numButton)
-})
-
